bde4eca2222535432a04a3cd356a382bf2271034
Rinjani <kitasukakitasuka12@gmail.com>
2026-01-03 13:40:48 +0700

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,7 +1,9 @@
 
 export enum JobType {
   SINGLE = "Satu pesanan satu produk total satu pesanan",
-  TRIPLE = "Satu pesanan tiga produk total tiga pesanan"
+  TRIPLE = "Satu pesanan tiga produk total tiga pesanan",
+  QUAD =  "Satu pesanan empat produk total empat pesanan",
+  PENTA = "Satu pesanan lima produk total lima pesanan"
 }
 
 export interface TaskData {
